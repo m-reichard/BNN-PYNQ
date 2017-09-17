f60821a9b7f165a2265bf74e52bce951ee448946
Yaman Umuroglu <maltanar@gmail.com>
2017-09-17 22:50:52 +0000

    don't redownload fashion-mnist if not necessary

--- bnn/src/training/fashion-mnist-train/download_trained_params.sh
+++ bnn/src/training/fashion-mnist-train/download_trained_params.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget -O "fashion_mnist_parameters.npz" "https://drive.google.com/uc?export=download&id=0B5p_4p8i3zguazJRWUxpOTV2OG8"
+wget -nc -O "fashion_mnist_parameters.npz" "https://drive.google.com/uc?export=download&id=0B5p_4p8i3zguazJRWUxpOTV2OG8"
